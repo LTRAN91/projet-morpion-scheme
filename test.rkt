82aeb00f9b785201fa4dd81826db3f6f5fc1b23d
LTRAN91 <tlinh91xp@gmail.com>
2016-12-13 09:30:45 +0100

    Add files via upload

--- test.rkt
+++ test.rkt
@@ -1,67 +1,51 @@
 #lang racket
 
-; https://www.moparscape.org/community/general-programming/158/racket-tic-tac-toe/633701
-
 (define (new-board sz)
-  (define (new-row)(build-list sz (lambda(l)" ")))
+  (define (new-row)(build-list sz (lambda(l) 0 )))
   (build-list sz (lambda(l)(new-row))))
 
 (new-board 10)
 
-(define (board-at board x y)
-  (list-ref (list-ref board y) x)) ;cible case
+;(define (board-at board x y)
+;  (list-ref (list-ref board y) x)) ;cible case
 
 
-(board-at (new-board 10) 2 3)
+;(board-at (new-board 10) 2 3)
 
-(define (is-avail? board x y)
-  (string=? (board-at board x y)" ")) ;vérifie que case est bien vide
+(define (joueurs n)
+        (if ( = n 1)
+          (begin
+            (display " Au tour de joueur 1")
+            (position (new-board 10) 1)
+            (joueurs (+ n 1)))
+          (begin
+            (display " Au tour de joueur 2")
+            (position (new-board 10) 2)
+            (joueurs (- n 1)))))
 
-(define (is-marked? board c x y)
-  (string=? (board-at board x y) c)) ;vérifie que case est rempli
+(define (position board j )
+  (display "choisir la ligne et la colonne: ")
+  (newline)
+  (let ((x (read)))
+    (let((y (read)))
+      (list-ref (list-ref board x) y)
+      (let ((z (list-ref (list-ref board x) y)))
+        (if( = j 1)
+           (lambda (z) 1)
+             
+           (lambda (z) 2))))))
 
-(define (is-win? board c)
-  (or(check-horiz? board c)(check-vert? board c)(check-diag? board c)))
+(define(remplace y board x )
+ ; (list-ref board x)
+  (if (= y 1)
+      (cons 1 (rest (list-ref board x)))
+      (cons (first (list-ref board x)) (remplace (- y 1)(rest board) x ))))
+(remplace 5 (new-board 10) 2)
 
+;trouver moyen de substituer " " par 1/2
+;(lambda (list-ref (list-ref board x) y) 1))))
 
-(define (check-horiz? board c)
-  (let((sz (length board)))
-    (<(count false?
-              (map (lambda(y)(has-filled-row? board c y))
-                   (range sz))) sz)))  ;(range 10) = (0 1 2 3...9)
-                                       ;(range 10 20) = (10 11 ... 19)
-(define (has-filled-row? board c y)
-  (=(count (lambda(l)
-              (string=? l c))(list-ref board y))(length board)))
-;length = compte nb éléments ds une liste
-;string=? = (string=? "a" "as" "a") #f
+;(position (new-board 10))
 
-
-(define (check-vert? board c)
-  (let((sz (length board)))
-    (<(count false?
-              (map (lambda(x)(has-filled-column? board c x))
-                   (range sz))) sz)))
- 
-(define (has-filled-column? board c x)
-  (let((sz (length board)))
-    (=(count (lambda(b)(not(false? b)))
-              (map (lambda(l)
-                     (string=? (list-ref (list-ref board l) x) c))
-                   (range sz))) sz)))
- 
-
-
-(define (check-diag? board c)
-  (let((sz (length board)))
-    (define (check-right-diag?)
-      (zero? 
-       (count false?
-              (map (lambda(n)
-                     (is-marked? board c n (- sz 1 n)))(range sz)))))
-    (define (check-left-diag?)
-      (zero?
-       (count false?
-              (map (lambda(n)
-                     (is-marked? board c n n))(range sz)))))
-    (or(check-right-diag?)(check-left-diag?))))
+;(joueurs 1)
+  
